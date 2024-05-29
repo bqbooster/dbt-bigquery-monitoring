0f66b3653fc31548cf5f00386413a830607f2b56
Christophe Oudar <kayrnt@gmail.com>
2024-05-29 23:03:18 +0200

    add models for storage and sqlmesh support

---
+++ models/storage/datamart/dataset_with_better_pricing_on_physical_billing_model.sql
@@ -0,0 +1,25 @@
+{{
+   config(
+    materialized='table',
+    )
+}}
+WITH storage_cost AS (
+  SELECT
+    project_id,
+    dataset_id,
+    SUM(total_logical_tb) AS total_logical_tb,
+    SUM(total_physical_tb) AS total_physical_tb,
+    SUM(logical_cost_monthly_forecast) AS logical_cost_monthly_forecast,
+    SUM(physical_cost_monthly_forecast) AS physical_cost_monthly_forecast,
+    SUM(storage_pricing_model_difference) AS storage_pricing_model_difference
+  FROM {{ ref('table_and_storage_with_cost') }}
+  WHERE total_physical_bytes > 0
+  AND table_type = 'BASE TABLE'
+  AND prefer_physical_pricing_model
+  GROUP BY ALL
+)
+SELECT
+  *
+FROM storage_cost
+ORDER BY storage_pricing_model_difference DESC
+LIMIT {{ var('output_limit_size') }}
---
+++ models/storage/datamart/table_with_better_pricing_on_physical_billing_model.sql
@@ -0,0 +1,25 @@
+{{
+   config(
+    materialized='table',
+    )
+}}
+WITH storage_cost AS (
+  SELECT
+    project_id,
+    dataset_id,
+    table_id,
+    total_logical_tb,
+    total_physical_tb,
+    logical_cost_monthly_forecast,
+    physical_cost_monthly_forecast,
+    storage_pricing_model_difference
+  FROM {{ ref('table_and_storage_with_cost') }}
+  WHERE total_physical_bytes > 0
+  AND table_type = 'BASE TABLE'
+  AND prefer_physical_pricing_model
+)
+SELECT
+  *
+FROM storage_cost
+ORDER BY storage_pricing_model_difference DESC
+LIMIT {{ var('output_limit_size') }}

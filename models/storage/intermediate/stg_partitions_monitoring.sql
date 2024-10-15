fce4a47420546f5daa61d6d6d0031c9120d2053a
Christophe Oudar <kayrnt@gmail.com>
2024-10-15 12:01:17 +0200

    Add partition_expiration_days to partition & move to view

--- models/storage/intermediate/stg_partitions_monitoring.sql
+++ models/storage/intermediate/stg_partitions_monitoring.sql
@@ -3,6 +3,12 @@
     materialized='table',
     )
 }}
+WITH partition_expirations AS (
+  SELECT table_catalog, table_schema, table_name, CAST(option_value AS INT64) AS partition_expiration_days
+  FROM {{ ref('information_schema_table_options') }}
+  WHERE option_name = 'partition_expiration_days'
+)
+
 SELECT
   TABLE_CATALOG AS project_id,
   TABLE_SCHEMA AS dataset_id,
@@ -13,10 +19,12 @@ SELECT
     WHEN regexp_contains(partition_id, '^[0-9]{8}$') THEN 'DAY'
     WHEN regexp_contains(partition_id, '^[0-9]{10}$') THEN 'HOUR'
     END AS partition_type,
+  partition_expiration_days,
   min(partition_id) AS earliest_partition_id,
   max(partition_id) AS latest_partition_id,
   count(partition_id) AS partition_count,
   sum(total_logical_bytes) AS sum_total_logical_bytes,
   max(last_modified_time) AS max_last_updated_time
-FROM {{ ref('information_schema_partitions') }}
+FROM {{ ref('information_schema_partitions') }} AS p
+JOIN partition_expirations AS e USING (table_catalog, table_schema, table_name)
 GROUP BY ALL

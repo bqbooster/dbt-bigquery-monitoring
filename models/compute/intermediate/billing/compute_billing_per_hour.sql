7c658272938f7c8ee0b6af673fde6643c9a77416
Christophe Oudar <kayrnt@gmail.com>
2024-11-08 16:42:48 +0100

    fix compute_billing_per_hour and add lookback_incremental_billing_window_days

--- models/compute/intermediate/billing/compute_billing_per_hour.sql
+++ models/compute/intermediate/billing/compute_billing_per_hour.sql
@@ -22,6 +22,6 @@ WHERE
 ((service.description LIKE 'BigQuery' AND LOWER(sku.description) LIKE '%analysis%')
 OR (service.description LIKE 'BigQuery Reservation API'))
 {% if is_incremental() %}
-AND TIMESTAMP_TRUNC(usage_start_time, HOUR) >= _dbt_max_partition
+AND TIMESTAMP_TRUNC(usage_start_time, HOUR) >= TIMESTAMP_SUB(_dbt_max_partition, INTERVAL {{ var('lookback_incremental_billing_window_days') }} DAY)
 {% endif %}
 GROUP BY ALL

f35fdb7a69bb53b85c66020f65d7cc00b547d436
Christophe Oudar <kayrnt@gmail.com>
2025-01-06 15:32:16 +0100

    DBT_BQ_MONITORING_GCP_PROJECTS shouldn't be required

--- macros/project_list.sql
+++ macros/project_list.sql
@@ -5,9 +5,13 @@
      {{ return(projects) }}
   {#-- check if it's the string and it contains a "," --#}
   {% elif projects is string %}
-     {% set projects_replaced = projects | replace("'", '"') %}
-     {% set json = fromjson('{"v":' ~ projects_replaced ~ '}') %}
-    {{ return (json['v']) }}
+     {% if projects == '' %}
+       {{ return([]) }}
+     {% else %}
+       {% set projects_replaced = projects | replace("'", '"') %}
+       {% set json = fromjson('{"v":' ~ projects_replaced ~ '}') %}
+       {{ return (json['v']) }}
+     {% endif %}
   {% else %}
     {{ exceptions.raise_compiler_error('Invalid `input_gcp_projects` variables. Got: ' ~ input_gcp_projects ~ ' but expected form like input_gcp_projects = ["project_1", "project_2"] ') }}
   {% endif %}

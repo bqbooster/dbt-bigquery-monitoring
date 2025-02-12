0abff97e0d9e290175318996fd8d64b68048e532
Christophe Oudar <kayrnt@gmail.com>
2025-02-12 14:08:14 +0100

    Fix query_with_better_pricing views formula

--- models/monitoring/compute/datamart/job/query_with_better_pricing_using_flat_pricing_view.sql
+++ models/monitoring/compute/datamart/job/query_with_better_pricing_using_flat_pricing_view.sql
@@ -6,8 +6,9 @@
 SELECT
   *,
   ondemand_query_cost - flat_pricing_query_cost AS cost_savings,
-  1 - flat_pricing_query_cost / ondemand_query_cost AS cost_savings_pct
+  1 - SAFE_DIVIDE(ondemand_query_cost, flat_pricing_query_cost) AS cost_savings_pct
 FROM
   {{ ref('jobs_by_project_with_cost') }}
 WHERE
-ondemand_query_cost <= flat_pricing_query_cost
+  ondemand_query_cost > flat_pricing_query_cost
+  AND flat_pricing_query_cost > 0
--- models/monitoring/compute/datamart/job/query_with_better_pricing_using_on_demand_view.sql
+++ models/monitoring/compute/datamart/job/query_with_better_pricing_using_on_demand_view.sql
@@ -6,8 +6,9 @@
 SELECT
   *,
   flat_pricing_query_cost - ondemand_query_cost AS cost_savings,
-  1 - ondemand_query_cost / flat_pricing_query_cost AS cost_savings_pct
+  1 - SAFE_DIVIDE(flat_pricing_query_cost, ondemand_query_cost) AS cost_savings_pct
 FROM
   {{ ref('jobs_by_project_with_cost') }}
 WHERE
-ondemand_query_cost <= flat_pricing_query_cost
+  flat_pricing_query_cost > ondemand_query_cost
+  AND ondemand_query_cost > 0

3e4e7c26ccc0aa66a6b1cee47ff359356ea48f12
Christophe Oudar <kayrnt@gmail.com>
2025-02-20 00:54:30 +0100

    fix jobs_done_incremental_hourly macro and remove jobs_done_incremental_minute macro

--- macros/jobs_done_incremental_hourly.sql
+++ macros/jobs_done_incremental_hourly.sql
@@ -2,26 +2,57 @@
     The downstream jobs will use that model to aggregate all finished jobs as pending/running jobs metrics will evolve
     So the lookback is based on the max partition of the downstream model
 #}
+
+{#
+  Returns the lower boundary timestamp when no data is available.
+#}
+{% macro lower_boundary_no_data() -%}
+    TIMESTAMP_SUB(
+        TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), HOUR),
+        INTERVAL {{ var('lookback_window_days') }} DAY
+    )
+{%- endmacro %}
+
+{#
+  Returns the partition timestamp.
+#}
+{% macro get_partition_timestamp() -%}
+    TIMESTAMP_TRUNC(
+        CASE
+            WHEN _dbt_max_partition IS NULL THEN {{ lower_boundary_no_data() }}
+            ELSE _dbt_max_partition
+        END,
+        HOUR
+    )
+{%- endmacro %}
+
+{#
+  Returns done jobs within the specified time boundaries.
+#}
 {% macro jobs_done_incremental_hourly() -%}
-(SELECT *
-FROM
-  {{ ref('jobs_with_cost') }}
-WHERE
-{% if is_incremental() %}
-creation_time >= TIMESTAMP_TRUNC(_dbt_max_partition, HOUR)
-  {%- if enable_gcp_bigquery_audit_logs() %}
-AND hour BETWEEN TIMESTAMP_SUB(TIMESTAMP_TRUNC(_dbt_max_partition, HOUR), INTERVAL 6 HOUR) AND TIMESTAMP_TRUNC(_dbt_max_partition, HOUR)
-  {% endif %}
-{% else %}
-creation_time >= TIMESTAMP_SUB(
-  TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), HOUR),
-  INTERVAL {{ var('lookback_window_days') }} DAY)
-  {%- if enable_gcp_bigquery_audit_logs() %}
-AND hour >= TIMESTAMP_SUB(TIMESTAMP_TRUNC(TIMESTAMP_SUB(
-TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), HOUR),
-INTERVAL {{ var('lookback_window_days') }} DAY), HOUR), INTERVAL 6 HOUR)
-  {% endif %}
-{% endif %}
-AND state = 'DONE'
-)
+    (
+      SELECT *
+      FROM {{ ref('jobs_with_cost') }}
+      WHERE
+          {#- the table already exists, we read logs are above latest max partition or above lookback window #}
+          {% if is_incremental() %}
+            creation_time >= {{ get_partition_timestamp() }}
+            {#- and if we enabled audit logs, we pushback to 6h before because of the potential delay of audit logs #}
+            {% if enable_gcp_bigquery_audit_logs() %}
+              AND hour BETWEEN TIMESTAMP_SUB({{ get_partition_timestamp() }}, INTERVAL 6 HOUR)
+                         AND {{ get_partition_timestamp() }}
+            {% endif %}
+          {#- if the table doesn't exist, we start from the lookback window #}
+          {% else %}
+            creation_time >= {{ lower_boundary_no_data() }}
+            {#- if we enabled audit logs, we pushback to 6h before because of the potential delay of audit logs #}
+            {% if enable_gcp_bigquery_audit_logs() %}
+              AND hour >= TIMESTAMP_SUB(
+                           TIMESTAMP_TRUNC({{ lower_boundary_no_data() }}, HOUR),
+                           INTERVAL 6 HOUR
+                         )
+            {% endif %}
+          {% endif %}
+          AND state = 'DONE'
+    )
 {%- endmacro %}

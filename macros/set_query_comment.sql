2fed34cd39c9a569ba770f847411ea12b438dfb0
Kayrnt <Kayrnt@users.noreply.github.com>
2025-03-25 21:40:53 +0000

    Release v0.19.0

--- macros/set_query_comment.sql
+++ macros/set_query_comment.sql
@@ -6,7 +6,7 @@
     {%- set comment_dict = {} -%}
     {%- do comment_dict.update(
         app='dbt',
-        dbt_bigquery_monitoring_version="0.18.4",
+        dbt_bigquery_monitoring_version="0.19.0",
         dbt_version=dbt_version | default(''),
         project_name=project_name | default(''),
         target_name=target.name | default(''),

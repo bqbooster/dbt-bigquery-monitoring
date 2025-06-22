94c27451938258eb69adfaad69a27f0de530ccaf
Christophe Oudar <kayrnt@gmail.com>
2025-06-22 17:22:17 +0200

    Update materialization logic to return 'project_by_project_view' when explicit projects are provided, ensuring proper handling of project references in views.

--- macros/materialization_view_if_explicit_projects.sql
+++ macros/materialization_view_if_explicit_projects.sql
@@ -1,12 +1,12 @@
-{#- check if var('projects') is defined to materialize as a view if we declare explicit project else an ephemeral
+{#- check if we pass explicitly projects (project mode) to materialize as a view using project references.
     It avoids following BQ error:
     Within a standard SQL view, references to tables/views require explicit project IDs
     unless the entity is created in the same project that is issuing the query,
     but these references are not project-qualified: "region-XXX.INFORMATION_SCHEMA.XXX"
  -#}
 {% macro materialized_as_view_if_explicit_projects() -%}
-  {% if project_list() | length == 0 %}
-    {{ return('ephemeral') }}
+  {% if project_list() | length > 0 %}
+    {{ return('project_by_project_view') }}
   {% else %}
     {{ return('view') }}
   {% endif %}

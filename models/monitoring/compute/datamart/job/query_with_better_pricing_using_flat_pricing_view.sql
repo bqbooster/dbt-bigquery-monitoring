32f7e72995f5d2da881c243d93cc1ba3075c0ed2
Christophe Oudar <90898980+github-christophe-oudar@users.noreply.github.com>
2025-09-21 10:50:16 +0200

    Refactor SQL models to use jobs_with_cost instead of jobs_by_project_with_cost for improved clarity and performance. This change flattens models and updates relevant queries accordingly. (#148)
    Co-authored-by: Christophe Oudar <kayrnt@gmail.com>

--- models/monitoring/compute/datamart/job/query_with_better_pricing_using_flat_pricing_view.sql
+++ models/monitoring/compute/datamart/job/query_with_better_pricing_using_flat_pricing_view.sql
@@ -8,7 +8,7 @@ SELECT
   ondemand_query_cost - flat_pricing_query_cost AS cost_savings,
   1 - SAFE_DIVIDE(ondemand_query_cost, flat_pricing_query_cost) AS cost_savings_pct
 FROM
-  {{ ref('jobs_by_project_with_cost') }}
+  {{ ref('jobs_with_cost') }}
 WHERE
   ondemand_query_cost > flat_pricing_query_cost
   AND flat_pricing_query_cost > 0
--- models/monitoring/compute/datamart/job/query_with_better_pricing_using_on_demand_view.sql
+++ models/monitoring/compute/datamart/job/query_with_better_pricing_using_on_demand_view.sql
@@ -8,7 +8,7 @@ SELECT
   flat_pricing_query_cost - ondemand_query_cost AS cost_savings,
   1 - SAFE_DIVIDE(flat_pricing_query_cost, ondemand_query_cost) AS cost_savings_pct
 FROM
-  {{ ref('jobs_by_project_with_cost') }}
+  {{ ref('jobs_with_cost') }}
 WHERE
   flat_pricing_query_cost > ondemand_query_cost
   AND ondemand_query_cost > 0
